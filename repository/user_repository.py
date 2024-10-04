736914cb8a0197743542a7bb6fbf6d5251f89a44
Taielmolina01 <tamolina@fi.uba.ar>
2024-10-04 13:59:49 -0300

    some things more maybe

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -12,8 +12,16 @@ class UserRepository:
         self.db.refresh(user)
         return user
     
+    def get_users(self) -> list[UserBase]:
+        return self.db.query(UserBase).all()
+
     def get_user(self, user_id: int) -> UserBase:
-        self.db.query(UserBase).filter(UserBase.user_id == user_id).first()
+        return self.db.query(UserBase).filter(UserBase.user_id == user_id).first()
+    
+    def update_user(self, user: UserBase) -> UserBase:
+        self.db.commit()
+        self.db.refresh(user)
+        return user
 
     def delete_user(self, user: UserBase) -> bool:
         self.db.delete(user)
--- service/exceptions/users_exceptions.py
+++ service/exceptions/users_exceptions.py
@@ -1,5 +1,6 @@
 MESSAGE_USER_NOT_REGISTERED = "El usuario de id {user_id} no existe"
 MESSAGE_USER_HAVE_NOT_NAME = "El usuario debe poseer un nombre"
+MESSAGE_USER_ALREADY_REGISTERED = "El usuario de id {user_id} ya existe"
 
 class UserNotRegistered(Exception):
     def __init__(self, user_id):
@@ -11,3 +12,7 @@ class UserWithoutName(Exception):
         self.message = MESSAGE_USER_HAVE_NOT_NAME
         super().__init__(self.message)
 
+class UserAlreadyRegistered(Exception):
+    def __init__(self, user_id):
+        self.message = MESSAGE_USER_ALREADY_REGISTERED.format(user_id)
+        super().__init__(self.message)
